a5e15e4a2f5a74f4d3eb3b0ed1e29b9382bbf26a
Scott Davis <sfdavis0@gmail.com>
2021-05-05 21:32:30 -0400

    updating scraper to fully run and code formatting

--- cfb_comments.py
+++ cfb_comments.py
@@ -5,27 +5,31 @@ import requests
 import redAPI
 
 # Reddit stuff
-r = praw.Reddit(user_agent = "ARTCbot 1.3.0 by herumph",
-    client_id = ID,
-    client_secret = SECRET,
-    username = REDDIT_USERNAME,
-    password = REDDIT_PASS)
+r = praw.Reddit(
+    user_agent="ARTCbot 1.3.0 by herumph",
+    client_id=ID,
+    client_secret=SECRET,
+    username=REDDIT_USERNAME,
+    password=REDDIT_PASS,
+)
 
-'''headers = {'user_agent': "ARTCbot 1.3.0 by herumph",
+"""headers = {'user_agent': "ARTCbot 1.3.0 by herumph",
     'client_id': ID,
     'client_secret': SECRET,
     'username': REDDIT_USERNAME,
-    'password': REDDIT_PASS}'''
+    'password': REDDIT_PASS}"""
 
-def main(subreddit,id):
-  submission = r.submission(id=id)
-  print('Total comments to load '+str(submission.num_comments))
-  # this is super slow, not realistic to run, need another solution
-  submission.comments.replace_more(limit=None)
-  for comment in submission.comments.list():
-    print(comment.body)
-  print(len(submission.comments.list()))
 
-if __name__ == '__main__':
-  print('Starting parse...')
-  main('cfb','j8q4uh')
+def main(subreddit, id):
+    submission = r.submission(id=id)
+    print("Total comments to load " + str(submission.num_comments))
+    # this is super slow, not realistic to run, need another solution
+    submission.comments.replace_more(limit=None)
+    for comment in submission.comments.list():
+        print(comment.body)
+    print(len(submission.comments.list()))
+
+
+if __name__ == "__main__":
+    print("Starting parse...")
+    main("cfb", "j8q4uh")
--- scraper.py
+++ scraper.py
@@ -4,50 +4,66 @@ import pandas as pd
 from time import sleep
 import re
 
-def getGames(year,team):
-  team_url = '-'.join(team.lower().split())
-  team_url = re.sub('[()]', '', team_url)
-  url = 'https://www.sports-reference.com/cfb/schools/'+team_url+'/'+str(year)+'-schedule.html'
-  r = requests.get(url)
 
-  if r.status_code == 200:
-    soup = BeautifulSoup(r.content, 'html.parser')
-    for x in soup.find_all('td', class_='left'):
-      if 'opp_name' in str(x):
-        opp = x.text
-        if opp[1].isdigit(): # have to do this because of teams like Miami (FL)
-          opp = opp.split(')\xa0')[1]
-        print(opp)
-      if 'game_location' in str(x):
-        loc = x.text
-        if loc == '@':
-          loc = 'A'
-        if loc == '':
-          loc = 'H'
-        print(loc)
-      if 'date_game' in str(x):
-        date = x.text
-        print(date)
+def getGames(year, team):
+    team_url = "-".join(team.lower().split())
+    team_url = re.sub("[()]", "", team_url)
+    url = (
+        "https://www.sports-reference.com/cfb/schools/"
+        + team_url
+        + "/"
+        + str(year)
+        + "-schedule.html"
+    )
+    r = requests.get(url)
 
-      # how to format this data to return and save for look up
-    return 0
-  else:
-    return None
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.content, "html.parser")
+        teamDict = {"team": team, "opponent": [], "date": [], "location": []}
 
-if __name__ == '__main__':
-  year = 2020
+        for x in soup.find_all("td", class_="left"):
+            if "opp_name" in str(x):
+                opp = x.text
+                if opp[1].isdigit():  # have to do this because of teams like Miami (FL)
+                    opp = opp.split(")\xa0")[1]
+                teamDict["opponent"].append(opp)
+            if "game_location" in str(x):
+                loc = x.text
+                if loc == "@":
+                    loc = "A"
+                if loc == "":
+                    loc = "H"
+                teamDict["location"].append(loc)
+            if "date_game" in str(x):
+                date = x.text
+                teamDict["date"].append(date)
 
-  teamdf = pd.read_csv('teams.csv')
-  teams = [x.split(';') for x in teamdf['teams'].values]
-  teams = [item for sublist in teams for item in sublist]
+        return teamDict
+    else:
+        return None
 
-  noData = []
-  for team in teams:
-    print(team)
-    games = getGames(year,team)
-    if games is None:
-      noData.append(team)
-    break
-    sleep(5)
 
-  print('Teams without data: ',noData)
+if __name__ == "__main__":
+    year = 2020
+
+    teamdf = pd.read_csv("teams.csv")
+    teams = [x.split(";") for x in teamdf["teams"].values]
+    teams = [item for sublist in teams for item in sublist]
+
+    noData = []
+    df = pd.DataFrame()
+    for team in teams:
+        print(team)
+        teamDict = getGames(year, team)
+        print(teamDict)
+        if teamDict is None:
+            noData.append(team)
+        else:
+            joinKeys = ["opponent", "date", "location"]
+            for key in joinKeys:
+                teamDict[key] = "/".join(teamDict[key])
+            df = df.append(teamDict, ignore_index=True)
+        df.to_csv(str(year) + "_games.csv")
+        sleep(2)  # to not spam the website too much
+
+    print("Teams without data: ", noData)

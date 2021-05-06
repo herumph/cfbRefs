5dd7ba476fc1970969659294c3ab72f6b481ab5c
Sizemo <aesizemore@icloud.com>
2021-05-06 14:36:44 -0500

    first attempt at judging comments

--- judgeComments.py
+++ judgeComments.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pyinputplus as pyip
 
 trainingGames = [
     "clemson_tosu2019",
@@ -19,10 +20,21 @@ for game in trainingGames:
         # the following variable is just a string of each reddit comment
         # so this is where you'll look at the string and see what's "in" it :)
         a = row["body"]
+        try:
+            if commentdf[commentdf[a].str.contains(r"ref|call|flag|penalty|review|targeting|holding|offsides|horse|collar|facemask|fumble|wasn\'t down|out of bounds|ruled|rule|ruling|roughing|passer|ejected|replay|refs|interference|blind|bullshit|screwed|called|fucked|biased|illegal|late|calling", case=False, regex=False, na=False)] == True:
+                print(row) 
+                judgment = pyip.inputInt("""Is the comment:
+                1. Not about the refs
+                2. Expressing positive sentiment toward the refs
+                3. Expressing negative sentiment toward the refs""", min=1, max=3)
+                commentType.append(judgment)
+            else:
+                commentType.append(0)
+        except:
+            commentType.append(0)
 
         # after you get what type of comment it is, e.g. not about the refs, good refs, bads ref
         # append the type to commentType (type can be whatever you want, string/int/whatever)
-        commentType.append(0)
 
     # now that each comment was judged we add a new column to the dataframe with the judgement
     # and save the edited dataframe
